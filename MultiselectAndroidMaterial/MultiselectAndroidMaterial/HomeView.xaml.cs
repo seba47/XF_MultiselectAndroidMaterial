13816bebd5b25a1053bd349a4930f50c5dffc7f9
Sebastian Cabrera <scabrera@carrasco.uruit.com>
2017-04-26 10:39:27 -0300

    First push - Xamarin Forms Listview for Android 1.0

---
+++ MultiselectAndroidMaterial/MultiselectAndroidMaterial/HomeView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using MultiselectAndroidMaterial.CustomControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MultiselectAndroidMaterial
+{
+    public partial class HomeView : ContentPage
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+            LoadTestData();
+        }
+
+        private void LoadTestData()
+        {
+            //MultiSelectListView
+            List<MultiselectListViewItem> itemsList = new List<CustomControls.MultiselectListViewItem>();
+            for (int i = 0; i < 30; i++)
+            {
+                MultiselectListViewItem item = new CustomControls.MultiselectListViewItem("Test " + i);
+                itemsList.Add(item);
+            }
+
+            MultiSelectListView.ItemsSource = itemsList;
+        }
+    }
+
+   
+}
